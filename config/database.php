a21a99701d06bf07f35c788d68f093eb00076649
salah <salahalhimyari2023@gmail.com>
2025-08-19 04:36:27 +0300

    feat(database): add MySQL PDO connection class with error handling

--- config/database.php
+++ config/database.php
@@ -12,7 +12,7 @@ class Database {
 
         try {
             $this->conn = new PDO(
-                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                'mysql:host='. $this->host . ';dbname=' . $this->db_name,
                 $this->username, 
                 $this->password
             );
