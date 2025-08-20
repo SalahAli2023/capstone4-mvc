2bcc2ae03734dfcaee19e85c7cf96b825ad43ed6
salah <salahalhimyari2023@gmail.com>
2025-08-21 00:08:53 +0300

    feat(auth): add UserController class & implement authentication system

---
+++ app/controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserController extends Controller {
+    private $userModel;
+
+    public function __construct() {
+        $this->userModel = new User();
+    }
+
+    public function index() {
+        if (!isset($_SESSION['user_id'])) {
+            header('Location: /login');
+            exit();
+        }
+
+        $users = $this->userModel->getAllUsers();
+        $this->view('users/index', ['users' => $users]);
+    }
+}
